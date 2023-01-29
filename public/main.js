ad62117b3dfafc8a6f78d3acdb769be2a0a8a718
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2023-01-29 17:42:52 +0000

    Html Divs

---
+++ public/main.js
@@ -0,0 +1,29 @@
+let myQuestions = [
+    {
+        question: 'and I almost brought him up',
+        correctAnswer: 'but you start to talk about the movies',
+    },
+    {
+        question: 'morning his place',
+        correctAnswer: 'burnt toast sunday',
+    },
+    {
+        question: 'got the radio on my old blue jeans',
+        correctAnswer: 'and I\'m wearing my heart on my sleeve',
+    },
+    {
+        question: 'I greet you with a battle hero\'s welcome',
+        correctAnswer: 'I take your indiscretions all in good fun',
+    },
+    {
+        question: 'and he feels like home',
+        correctAnswer: 'if the shoe fits walk in it everywhere you go',
+    },
+    {
+        question: 'but she wears short skirts',
+        correctAnswer: 'I wear tshirts',
+    },
+]
+
+
+ 
