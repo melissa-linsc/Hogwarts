c0053b4d885216b9e2ea975e0b6ceabbf1acafb2
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-03-04 17:46:32 +0000

    tour dates, TTPD sections

---
+++ index.js
@@ -0,0 +1,27 @@
+//countdown timer
+
+const countDownDate = new Date("Apr 19, 2024 00:00:00").getTime()
+
+const x = setInterval( function(){
+    let now = new Date().getTime()
+
+    let distance = countDownDate - now 
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+},)
+
+
+
+
